1edc79007f7e4868f7f3c44d17db0e37e445a7e0
zxy <zxy18310546374@163.com>
2019-09-19 14:49:42 +0800

    takeOut

---
+++ take-domain/src/main/java/com/mr/takedomain/TakeDomainApplication.java
@@ -0,0 +1,13 @@
+package com.mr.takedomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeDomainApplication.class, args);
+    }
+
+}
---
+++ take-domain/src/main/java/com/mr/takedomain/domain/Keep.java
@@ -0,0 +1,11 @@
+package com.mr.takedomain.domain;
+
+import lombok.Data;
+
+@Data
+public class Keep {
+    private Integer kId;
+    private String kName;
+    private Integer userId;
+    private Integer storeId;
+}
